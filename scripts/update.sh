bce2caa29a4eefd4e158b260664fc9353e2e6a87
alberto56 <albert@mediatribe.net>
2020-03-15 10:42:36 -0400

    More complete update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,10 @@
 #
 # Update Jenkins with SSL
 #
+set -e
+git pull origin master
 docker pull jenkins/jenkins:lts
 docker-compose -f docker-compose.yml \
   -f docker-compose.ssl.yml up -d
+docker restart nginx-proxy
+docker restart nginx-letsencrypt
