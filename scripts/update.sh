96d64a9aaebeed318c48dbfd8af57799813a8dec
Albert Albala <albert@mediatribe.net>
2023-05-12 11:11:23 -0400

    Use dcycle/jenkins:1 as a base, instead of installing Python ourselves (#13)

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@
 #
 set -e
 git pull origin master
-docker pull jenkins/jenkins:latest
+docker pull dcycle/jenkins:1
 docker-compose -f docker-compose.yml \
   -f docker-compose.ssl.yml up -d
 docker restart nginx-proxy
